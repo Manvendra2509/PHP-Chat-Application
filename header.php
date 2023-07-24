799c6d41d1e9a18e1dca2a03115d877bc6a73c54
Manvendra2509 <manvendra.17cse468@rtu.ac.in>
2023-07-24 19:21:04 +0530

    New activity log formatting

--- header.php
+++ header.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Realtime Chat App</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="style.css">
-  
+
+
 </head>
--- php/activity_data.php
+++ php/activity_data.php
@@ -1,15 +1,18 @@
 <?php
+$total_rows = mysqli_num_rows($query);
+$current_row = 1;
 while ($row = mysqli_fetch_assoc($query)) {
 
     $timestamp = date("d M y, h:i A", strtotime($row['timestamp']));
     $output .= '
                     <div class="content">
-                        <div class="dot"></div>
+                        <div class="indicator"><div class="dot"></div> ' . ($current_row !== $total_rows ? '<div class="line"></div>' : '') . '</div>
                         <div class="details sessioncard">
                         <div> ' . date("d M y, h:i A", strtotime($row['timestamp'])) . '</div>
                         ' . $row['activity_description'] . '
                         </div>
                     </div>
                 ';
+    $current_row++;
 }
 ?>
